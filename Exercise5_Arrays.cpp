2faa0191b7aa4c714cd3b04b026b5aee0b172f55
Kevin-Frias <KevinFriasRuiz48@gmail.com>
2024-07-30 06:37:19 -0500

    Ares
    Section 8 on video 58.

---
+++ Exercise5_Arrays.cpp
@@ -0,0 +1,12 @@
+/*Declare an array of 10  integers named arr  and initialize the array so that all 10 integers are 0 .
+Then assign 100  to the first element of the array and 1000  to the last element of the array.
+*/
+
+
+int main() {
+    int arr[10]{0};
+    arr[0] = 100;
+    arr[9] = 1000;
+
+    return 0;
+}
